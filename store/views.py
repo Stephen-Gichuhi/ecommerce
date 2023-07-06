b90dbdb1c3ad9419493da384e214b9f6bd4a05a2
willymwaura <wilsonmwaura697@gmail.com>
2023-07-06 21:46:39 +0300

    first commit

---
+++ store/views.py
@@ -0,0 +1,174 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+import json
+import datetime
+from .models import * 
+from .utils import cookieCart, cartData, guestOrder
+
+def store(request):
+	data = cartData(request)
+
+	cartItems = data['cartItems']
+	order = data['order']
+	items = data['items']
+
+	products = Product.objects.all()
+	context = {'products':products, 'cartItems':cartItems}
+	return render(request, 'store/store.html', context)
+from django.shortcuts import get_object_or_404
+
+def lipa(request):
+	return render(request, 'store/lipa.html')
+
+def product(request, id):
+    data = cartData(request)
+    
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
+    
+    product = get_object_or_404(Product, id=id)
+    
+    context = {
+        'product': product,
+        'cartItems': cartItems
+    }
+    
+    return render(request, 'store/product.html', context)
+
+
+def cart(request):
+	data = cartData(request)
+
+	cartItems = data['cartItems']
+	order = data['order']
+	items = data['items']
+
+	context = {'items':items, 'order':order, 'cartItems':cartItems}
+	return render(request, 'store/cart.html', context)
+
+def checkout(request):
+	data = cartData(request)
+	
+	cartItems = data['cartItems']
+	order = data['order']
+	items = data['items']
+
+	context = {'items':items, 'order':order, 'cartItems':cartItems}
+	return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+	data = json.loads(request.body)
+	productId = data['productId']
+	action = data['action']
+	print('Action:', action)
+	print('Product:', productId)
+
+	customer = request.user.customer
+	product = Product.objects.get(id=productId)
+	order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+	orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+	if action == 'add':
+		orderItem.quantity = (orderItem.quantity + 1)
+	elif action == 'remove':
+		orderItem.quantity = (orderItem.quantity - 1)
+
+	orderItem.save()
+
+	if orderItem.quantity <= 0:
+		orderItem.delete()
+
+	return JsonResponse('Item was added', safe=False)
+
+def processOrder(request):
+	transaction_id = datetime.datetime.now().timestamp()
+	data = json.loads(request.body)
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+	else:
+		customer, order = guestOrder(request, data)
+
+	total = float(data['form']['total'])
+	order.transaction_id = transaction_id
+
+	if total == order.get_cart_total:
+		order.complete = True
+	order.save()
+
+	if order.shipping == True:
+		ShippingAddress.objects.create(
+		customer=customer,
+		order=order,
+		address=data['shipping']['address'],
+		city=data['shipping']['city'],
+		state=data['shipping']['state'],
+		zipcode=data['shipping']['zipcode'],
+		)
+
+	return JsonResponse('Payment submitted..', safe=False)
+
+from django.shortcuts import render
+ 
+from .mpesa_credentials import LipanaMpesaPpassword , MpesaAccessToken 
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+from django.http import response ,HttpResponse
+
+
+
+# Create your views here.
+
+def lipa_na_mpesa(request):
+               
+        #phone="254112100378"
+        #Amount=1
+        Amount=request.POST['amount']
+		
+        
+        phone=request.POST['number']
+        print(phone)
+        Amount=int(float(Amount))
+		
+		
+        
+        print(phone)
+		
+        access_token = MpesaAccessToken.validated_mpesa_access_token
+        
+
+        api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+        headers = {"Authorization": "Bearer %s" % access_token}
+        request = {
+            "BusinessShortCode": LipanaMpesaPpassword.Business_short_code,
+            "Password": LipanaMpesaPpassword.decode_password,
+            "Timestamp": LipanaMpesaPpassword.lipa_time,
+            "TransactionType": "CustomerPayBillOnline",
+            "Amount":Amount,
+            "PartyA":phone,
+            "PartyB": LipanaMpesaPpassword.Business_short_code,
+            "PhoneNumber":phone,
+            "CallBackURL": "https://kenyathena.onrender.com/payment/callback",
+            "AccountReference": "JAMII ELECTRONICS ",
+            "TransactionDesc": "Testing stk push"
+        }
+        response = requests.post(api_url, json=request, headers=headers)
+        return HttpResponse("complete the transaction")
+
+def gettoken(request):
+        consumer_key = 'bdABIHJcBQA5ki4tRYQumvcLA8QaaDP3'
+        consumer_secret = 'QooVaV2gUsBcKziE'
+        api_URL = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
+        r = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret))
+        mpesa_access_token = json.loads(r.text)
+        validated_mpesa_access_token = mpesa_access_token['access_token']
+        return HttpResponse(validated_mpesa_access_token)
+
+def homepage(request):
+	return render (request, 'store/homepage.html')
+def visa(request):
+	return render (request, 'store/visa.html')
